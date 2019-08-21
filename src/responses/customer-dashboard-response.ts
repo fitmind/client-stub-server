523a82533b501387acc63c62f1d777c4be055c8f
Hemal Pandya <hemal@digiflux.io>
2019-08-21 14:31:52 +0530

    Dashboard response updated

--- src/responses/customer-dashboard-response.ts
+++ src/responses/customer-dashboard-response.ts
@@ -1,14 +1,4 @@
 const customerDashboardResponse = {
-  _id: '1234',
-  name: 'Fitmind User',
-  createdAt: new Date().toISOString().slice(0, 10),
-  description:
-    'II’m a versatile personal trainer, who has experience in Functional training, wieight training and yoga. I’ve been training people of all ages for over 12 years improving their overall quality of life.',
-  email: 'user1@fitmind.io',
-  interestedInExperiseAreas: ['PERSONAL_COACH', 'YOGA_COACH'],
-  phone: '+44 20 7123 1234',
-  pictureUrl:
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBx76lQwzW2cAsjz5JqgVp_ReTpVji6G_pMO6crXSJn9NETq3F',
   upcomingBookings: [
     {
       _id: '100',
